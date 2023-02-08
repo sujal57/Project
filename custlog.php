d9c94b3a9a742a2cf086891d6d05ff06d74948e9
dhruvesh26 <124428522+dhruvesh26@users.noreply.github.com>
2023-02-08 16:12:30 +0530

    Complete Login pages

---
+++ custlog.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page in HTML</title>
+    <link rel="stylesheet" href="csslogin.css">
+</head>
+<body>
+
+    <form action="">
+        <div class="heading">
+            <h3>Customer</h3>
+            <p>Login in with your username and password</p>
+        </div>
+
+        <!-- Main container for all inputs -->
+        <div class="Container1">
+            Your username
+            <input type="text" placeholder="Enter Username" name="aname" required>
+
+            <br><br>
+            Your password
+            <input type="password" placeholder="Enter Password" name="pwd" required>
+
+            <div class="Container2">
+                <label>
+                  <input type="checkbox" checked="checked" name="remember"> Remember me
+                </label>
+                <p class="fpwd"> <a href="#">Forgot Password?</a></p>
+            </div>
+
+            <button type="submit">Login</button>
+
+            <!-- Sign up link -->
+            <p class="reg">Not a member?  <a href="#">Register here!</a></p>
+
+        </div>
+
+    </form>
+</body>
+</html>
