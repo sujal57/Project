082c6b6f147dfffe4eb3ed15fb1dfd8e2d99e4e4
dhruvesh26 <124428522+dhruvesh26@users.noreply.github.com>
2023-02-25 23:10:38 +0530

    Update input type from text to email

--- adminlog.php
+++ adminlog.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login page in HTML</title>
     <link rel="stylesheet" href="csslogin.css">
 </head>
 <body>
 
-    <form action="">
+    <form action="adminverify.php" method="post">
         <div class="heading">
             <h3>Admin</h3>
             <p>Login in with your username and password</p>
@@ -18,7 +15,7 @@
         <!-- Main container for all inputs -->
         <div class="Container1">
             Your username
-            <input type="text" placeholder="Enter Username" name="aname" required>
+            <input type="email" placeholder="Enter Username" name="aname" required>
 
             <br><br>
             Your password
